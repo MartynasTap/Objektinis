bebcffd031df308d1e498310dde1198037c20eec
MartynasTap <145481815+MartynasTap@users.noreply.github.com>
2023-09-27 12:38:45 +0300

    Add files via upload

--- mylib.cpp
+++ mylib.cpp
@@ -16,12 +16,24 @@ void ivedimas(studentas &Temp){
         cout << "Iveskite egzamina: ";
         cin >> Temp.egz;
 }
-void vidurkis(studentas &Temp){
+void galutinis_vid(studentas &Temp){
     float k=0,j=0;
     for(auto a: Temp.paz){k=k+a; j++;}
-    Temp.rez=(Temp.egz+k)/(j+1);
+    Temp.rez_vid=(k/j)*0.4+Temp.egz*0.6;
 }
-void isvedimas(studentas &Temp){
+void isvedimas(studentas &Temp, float galutinis){
     cout << setw(15) << left <<Temp.var << setw(15) << left << Temp.pav;
-    printf("%-16.2f\n", Temp.rez);
+    printf("%-16.2f\n", galutinis);
+}
+void galutinis_med(studentas &Temp){
+    if (Temp.paz.size()%2==0){
+         nth_element(Temp.paz.begin(),Temp.paz.begin() + Temp.paz.size()/2,Temp.paz.end());
+         nth_element(Temp.paz.begin(),Temp.paz.begin() + (Temp.paz.size()-1)/2,Temp.paz.end());
+         Temp.rez_med=(Temp.paz[Temp.paz.size()/2]+Temp.paz[(Temp.paz.size()-1)/2])/2;
+    }
+    else {
+        nth_element(Temp.paz.begin(),Temp.paz.begin() + Temp.paz.size()/2,Temp.paz.end());
+        Temp.rez_med=Temp.paz[Temp.paz.size()/2];
+    }
+    Temp.rez_med=(Temp.rez_med*0.4)+(Temp.egz*0.6);
 }
